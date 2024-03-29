97ad3f8baf99f1a06fbb33a4ea6da7adc058d975
nightusk <nightusk@gmail.com>
2024-03-29 23:14:30 +0900

    emacs

---
+++ config/emacs/init.el
@@ -0,0 +1,34 @@
+(use-package autorevert
+  :custom
+  (global-autp-revert-mode t))
+
+(use-package cus-edit
+  :custom
+  (custom-file (locate-user-emacs-file "custom.el")))
+
+ (use-package custom
+  :custom
+  (custom-enabled-themes '(modus-vivendi))
+  (menu-bar-mode nil))
+
+(use-package elec-pair
+  :custom
+  (electric-pair-mode t))
+
+(use-package icomplete
+  :custom
+  (fido-vertical-mode t))
+
+(use-package simple
+  :custom
+  (column-number-mode t)
+  (normal-erase-is-backspace t))
+
+(use-package whitespace
+  :custom
+  (global-whitespace-mode t))
+
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
